c7b99952c4f49fbb6781a7e9f6f4063baf2a11ad
simon <simon.landis@gmail.com>
2017-02-09 10:40:37 -0600

    added image loop to events

--- IntegrationProject/Scripts/eventfilter.js
+++ IntegrationProject/Scripts/eventfilter.js
@@ -5,8 +5,8 @@
         app_key: "mbK7CNpm3twn8nJH",
         location: "San Francisco",
         q: eventType,
-        date: "Future",
-        page_size: 5
+        date: "This Week",
+        page_size: 25
     };
     EVDB.API.call("/events/search", oArgs, function (oData) {
         showResults(oData);
@@ -14,13 +14,18 @@
     function showResults(oData) {
         $('.table').find('tbody').empty();
         for (var i = 0; i < oData.events.event.length; i++) {
+            if (oData.events.event[i].image === null) {
+                i++;
+            } else {
 
-            $('.table').append(
-           //   '<tr><td>'+"<img src=" +oData.events.event[i].image.medium.url+ ">"+'</td>'+
-             '<tr><td>' + oData.events.event[i].title + '</td>' +
-             '<td>' + oData.events.event[i].modified + '</td>' +
-             '<td>' + oData.events.event[i].venue_name + '</td>' +
-             '<td>' + oData.events.event[i].venue_address + '</td></tr>')
+                $('.table').append(
+                 '<tr><td>'+"<img src=" +oData.events.event[i].image.medium.url+ ">"+'</td>'+
+                 '<td>' + oData.events.event[i].title + '</td>' +
+                 '<td>' + oData.events.event[i].modified + '</td>' +
+                 '<td>' + oData.events.event[i].venue_name + '</td>' +
+                 '<td>' + oData.events.event[i].venue_address + '</td></tr>')
+            }
         }
     }
 }
+
